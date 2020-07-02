71566badf5d81371bd17330964f6105bcee5aeb8
Michelangelo <michelangeloesposito24@gmail.com>
2020-07-02 21:44:23 +0200

    Aggiunto progetto iniziale

---
+++ Demetra/app/src/main/java/com/example/demetra/Promemoria.java
@@ -0,0 +1,14 @@
+package com.example.demetra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Promemoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promemoria);
+    }
+}
